514a04e00ef35a457dbb833fb972da68dc6ee5a3
Henrique Figueredo <162153493+Henriquefgr@users.noreply.github.com>
2024-07-04 13:54:59 -0300

    DIA 2 JS

---
+++ DAY02/index.js
@@ -0,0 +1,11 @@
+// variaveis let e const
+let nome ="Henrique Figueredo"
+console.log(nome)
+nome = 'The Diamond'
+console.log(nome)
+
+const myname ="Henrique Figueredo"
+console.log(myname)
+myname = 'The Diamond'
+console.log(myname)
+// const nao pode ser alterada dps de ser definida 
