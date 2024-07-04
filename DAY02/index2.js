95a4e578644759dbf96a6050c2ccdf4742fffe46
Henrique Figueredo <162153493+Henriquefgr@users.noreply.github.com>
2024-07-04 14:24:28 -0300

    fim do dia 2

---
+++ DAY02/index2.js
@@ -0,0 +1,10 @@
+// operadores aritmeticos
+let num1 = 2
+let num2 = 5
+
+let soma = num1 + num2;
+console.log(soma)
+
+// entrada do usario 
+let idade = parseInt(prompt('insira sua idade'))
+console.log(2024 - idade)
