7cb1e1f6054f54de9abf3fb18505a00fc97866c1
Henrique Figueredo <162153493+Henriquefgr@users.noreply.github.com>
2024-07-11 14:16:31 -0300

    DAY 11/07

---
+++ DAY06/index.js
@@ -0,0 +1,5 @@
+// estrutura de repeticao for 
+let tabuada = Number(prompt('escolha a tabuada que deseja'));
+for (let contador = 0; contador <= 30; contador++) {
+    console.log(tabuada + '*' + contador + " = " + tabuada * contador);
+}
---
+++ Exercicios/EX7/index.js
@@ -0,0 +1,4 @@
+let x 
+for( let x = 5; x <100; x *= 3){
+    console.log(x)
+}
