1980b664ed5ca06fc83bd65d0a80c83bba36c1eb
Henrique Figueredo <riquefgr@gmail.com>
2024-07-13 12:23:07 -0300

    ex 07

--- Exercicios/EX7/index.js
+++ Exercicios/EX7/index.js
@@ -1,4 +1,7 @@
-let x 
-for( let x = 5; x <100; x *= 3){
-    console.log(x)
+let numero = parseInt(prompt("digite um numero"))
+for(let i = numero; i <= numero + 2; i++){
+    console.log('tabuada do numero: ' + i)
+    for(let j = 0;  j <= 10; j++){
+        console.log(i + "x" + j + " = " + (i*j) )
+    }
 }
